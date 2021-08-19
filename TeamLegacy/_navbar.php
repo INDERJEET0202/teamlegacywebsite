0efe50d78f61f41925c90e9cf04d81622d40d052
INDERJEET <74589611+INDERJEET0202@users.noreply.github.com>
2021-08-19 15:18:35 +0530

    1st website

---
+++ TeamLegacy/_navbar.php
@@ -0,0 +1,54 @@
+<?php
+echo'
+<header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <span>TEAMLEGACY ESPORTS</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">HOME</a></li>
+          <!-- <li><a class="nav-link scrollto" href="inner-page.html">TOURNAMENT</a></li> -->
+          <li class="dropdown"><a href="#"><span>TOURNAMENT</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#services">Our Games</a></li>
+              <li><a href="inner-page.html">Running Tournaments</a></li>
+              <li><a href="#portfolio">Past Tournaments</a></li>
+            </ul>
+          </li> 
+          <li><a class="nav-link scrollto" href="blog.html">BLOGS</a></li>
+          <li><a class="nav-link scrollto" href="#footer">SOCIALS</a></li>
+          <li><a class="nav-link scrollto" href="#team">TEAM</a></li>
+          <li><a class="nav-link scrollto" href="#about">ABOUT US</a></li>
+           <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="inner-page.html">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <hr style="margin: 0px">
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>  -->
+          <li><a class="nav-link scrollto" href="#contact">CONTACT US</a></li>
+          <li><a class="getstarted scrollto" href="#about">LOGIN</a></li>
+          <li><a class="signup" href="#about">SIGNUP</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>'
+  ?>
+
